47f91a02cd8d81e244664fba8b2873447364536e
AMESocker <aesocher@yahoo.com>
2022-11-26 10:14:03 -0800

    added user routes

--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -1,19 +1,34 @@
 const router = require('express').Router();
 //api/users
     // GET all users
-router.get('/')
+router.get('/', (req, res) => {
+    res.send('<h1>All Users</h1>')
+} )
     // GET a single user by its _id and populated thought and friend data
-router.get('/:userId')
+router.get('/:userId', (req, res) => {
+    res.send('Single User')
+})
     // POST a new user:
-router.post('')
+router.post('/:userId', (req, res) => {
+    res.send('New User')
+})
         // example data {"username": "lernantino","email": "lernantino@gmail.com"}
     // PUT to update a user by its _id
-router.put('/')
+router.put('/:userId', (req, res) => {
+    res.send('Update User')
+})
     // DELETE to remove user by its _id
-router.delete('/')
+router.delete('/:userId', (req, res) => {
+    res.send('Delete User')
+})
+
 //api/users/:userId/friends/:friendId
     // POST to add a new friend to a user's friend list
-router.post('/')
+router.post('/:userId/friends/:friendId', (req, res) => {
+    res.send('New Friend')
+})
     // DELETE to remove a friend from a user's friend list
-router.delete('/')
+router.delete('/:userId/friends/:friendId', (req, res) => {
+    res.send('Remove Friend')
+})
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.use("/api/thoughts", apiThoughtsRoutes);
 router.use("/api/users", apiUserRoutes);
 
 router.use((req, res) =>
-  res.send(`<h1>Would it help if I got out and pushed? It might! `)
+  res.send(`<h1>Would it help if I got out and pushed? It might!</h1>`)
 );
 
 module.exports = router;
